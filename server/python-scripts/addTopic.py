70aa40e39570f9018ceb40eec03e73283ab8400f
Nikhil Pellakuru <76831568+nikrp@users.noreply.github.com>
2024-02-03 12:03:45 -0800

    ss

--- server/python-scripts/addTopic.py
+++ server/python-scripts/addTopic.py
@@ -5,11 +5,11 @@ os.chdir(r"C:\Users\nikrp\Documents\TutorSwift\server")
 conn = sqlite3.connect("topics.db")
 
 cursor = conn.cursor()
-cursor.execute("""CREATE TABLE IF NOT EXISTS topics (topic TEXT, budget INT, name TEXT, username TEXT)""")
+cursor.execute("""CREATE TABLE IF NOT EXISTS topics (topic TEXT, budget INT, name TEXT, username TEXT, profilePic TEXT)""")
 conn.commit()
 
-def addTopic(topic:str, budget:int, name:str, username:str):
-    cursor.execute("INSERT INTO topics (topic, budget, name, username) VALUES (?, ?, ?, ?)", (topic, budget, name, username, ))
+def addTopic(topic:str, budget:int, name:str, username:str, ppp:str):
+    cursor.execute("INSERT INTO topics (topic, budget, name, username, profilePic) VALUES (?, ?, ?, ?, ?)", (topic, budget, name, username, ppp))
     conn.commit()
 
 addTopic("mawth", 123, "jeffrey bezos", "jefboz")
