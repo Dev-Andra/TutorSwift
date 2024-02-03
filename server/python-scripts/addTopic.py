c8626022d7c5510bfe56311476e9add23d9ff484
Dev Andra <144872915+Dev-Andra@users.noreply.github.com>
2024-02-03 12:16:13 -0800

    Profile picture path

--- server/python-scripts/addTopic.py
+++ server/python-scripts/addTopic.py
@@ -1,15 +1,17 @@
 import sqlite3
 import os
+import profilePicture
 
-os.chdir(r"C:\Users\nikrp\Documents\TutorSwift\server")
+os.chdir(r"C:\Users\devpa\Desktop\TutorSwift-1\server")
 conn = sqlite3.connect("topics.db")
 
 cursor = conn.cursor()
 cursor.execute("""CREATE TABLE IF NOT EXISTS topics (topic TEXT, budget INT, name TEXT, username TEXT, profilePic TEXT)""")
 conn.commit()
 
-def addTopic(topic:str, budget:int, name:str, username:str, ppp:str):
-    cursor.execute("INSERT INTO topics (topic, budget, name, username, profilePic) VALUES (?, ?, ?, ?, ?)", (topic, budget, name, username, ppp))
+def addTopic(topic:str, budget:int, name:str, username:str):
+    ppp = profilePicture.findPic(username)
+    cursor.execute("INSERT INTO topics (topic, budget, name, username, profilePic) VALUES (?, ?, ?, ?, ?)", (topic, budget, name, username, ppp[0][0] + "." + ppp[0][1]))
     conn.commit()
     return 200
 
--- server/python-scripts/profilePicture.py
+++ server/python-scripts/profilePicture.py
@@ -2,8 +2,8 @@ import sqlite3
 import os
 import base64
 
-os.chdir(r"C:\Users\nikrp\Documents\TutorSwift\server")
-conn = sqlite3.connect("picture.sdb")
+os.chdir(r"C:\Users\devpa\Desktop\TutorSwift-1\server")
+conn = sqlite3.connect("picture.db")
 
 cursor = conn.cursor()
 cursor.execute("""CREATE TABLE IF NOT EXISTS pictures (name TEXT, type TEXT)""")
@@ -19,5 +19,4 @@ def findPic(name:str):
     list = cursor.fetchall()
     return list
 
-addPic('jefboz', "png")
-findPic('jefboz')
+print(findPic('jefboz'))
