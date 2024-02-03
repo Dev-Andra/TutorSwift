3e394e830634cbbe41a409ce6d838822c8d25588
Nikhil Pellakuru <76831568+nikrp@users.noreply.github.com>
2024-02-02 17:54:10 -0800

    Styling verfication email

--- client/src/navigation/NavigationMain.jsx
+++ client/src/navigation/NavigationMain.jsx
@@ -3,8 +3,8 @@ import { Outlet } from "react-router-dom"
 export default function NavigationMain() {
     return (
         <div className="w-screen">
-            <div className="flex flex-row w-4/6 mx-auto">
-                <h1 className={`text-5xl font-bold`}>Hi</h1>
+            <div className="flex flex-row w-4/6 mx-auto px-10 py-5 2xl:w-4/6 xl:w-4/6 l:w-4/6 md:w-5/6 sm:w-screen">
+                <h1 className={`text-5xl font-bold`}>TutorSwift</h1>
             </div>
             <Outlet/>
         </div>
--- server/python-scripts/emailVerification.py
+++ server/python-scripts/emailVerification.py
@@ -8,10 +8,23 @@ import sys
 EMAIL = "syrupisgreatagain@gmail.com"
 PASSWORD = "zhfmokmofebphdbl"
 
-#Function to SMPTLib servers and email a verification code
+#Function to SMTPLib servers and email a verification code
 def emailVerification(email:str):
     code = random.randrange(100000, 999999)
-    msg = MIMEText(str(code))
+    
+    # HTML formatting
+    html_message = f"""
+    <html>
+    <head></head>
+    <body style="width:100vw;padding-left: 10px; padding-right: 10px;">
+        <h1 style="color:palegreen;">TutorSwift</h1>
+        <p style="font-size: 14px;color:dodgerblue;font-weight:bold;">Below is your verification code for your TutorSwift account.</p>
+        <h1 style="font-weight:bolder;letter-spacing:3px;font-size:25px;">{code}</h1>
+    </body>
+    </html>
+    """
+    
+    msg = MIMEText(html_message, 'html')  # Set the message type to HTML
     msg["Subject"] = "Verify your TutorSwift email address"
     msg["From"] = EMAIL
     msg['To'] = email
